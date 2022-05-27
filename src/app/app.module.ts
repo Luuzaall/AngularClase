209bfdb2b3953b13a33f13be350b004c3656643c
Luuzaall <luuzaall@gmail.com>
2022-05-27 09:58:09 -0300

    Cambié app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './components/app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
+
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
-
-import { AppComponent } from './app.component';
-import { InicioComponent } from './inicio/inicio.component';
-import { ArticulosFamiliasComponent } from './articulo-familia/articulo-familia.component';
-import { MenuComponent } from './menu/menu.component';
-import { ArticulosComponent } from './articulos/articulos.component';
-import { HttpClientModule } from '@angular/common/http';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   imports: [
-    HttpClientModule,
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '', redirectTo: '/inicio', pathMatch: 'full' },
       { path: 'inicio', component: InicioComponent },
       { path: 'articulosfamilias', component: ArticulosFamiliasComponent },
-      { path: 'articulos', component: ArticulosComponent },
     ]),
   ],
   declarations: [
@@ -28,9 +28,9 @@ import { HttpClientModule } from '@angular/common/http';
     InicioComponent,
     ArticulosFamiliasComponent,
     MenuComponent,
-    ArticulosComponent,
   ],
   bootstrap: [AppComponent],
   providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
 })
 export class AppModule {}
+
